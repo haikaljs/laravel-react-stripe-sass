ebd4e9c7acb63142d2f33697655a6ad8c4157c30
haikaljs <haikaljavascript@gmail.com>
2024-12-02 03:52:41 +0800

    Create models and migrations

---
+++ app/Models/Feature.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Feature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['image', 'route_name', 'name', 'description', 'required_credits', 'active'];
+
+   
+}
